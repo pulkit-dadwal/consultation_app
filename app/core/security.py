2a50425e7a03763ff1493f62b2644d3ff9430402
Pulkit Dadwal <pulkitdadwal@Pulkits-Mac-mini.local>
2026-06-03 16:12:03 +0530

    implemented security.py with all JWT functions for token creation and validation

---
+++ app/core/security.py
@@ -0,0 +1,72 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated, Any, Optional
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+
+from app.core.config import settings
+from sqlalchemy.orm import Session
+
+from app.db.session import SessionLocal
+from app.models.user import User
+
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="/auth/token")
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str):
+    return bcrypt_context.hash(password)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+def authenticate_user(email: str, password: str, db):
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(email: str, user_id: str, role: str, expires_delta: timedelta):
+    encode = {'sub': email, 'id': user_id, 'role': role}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, settings.secret_key, algorithm=settings.algorithm)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+        email: str = payload.get('sub')
+        user_id: str = payload.get('id')
+        user_role: str = payload.get('role')
+        if email is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.')
+        return {'email': email, 'id': user_id, 'user_role': user_role}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
